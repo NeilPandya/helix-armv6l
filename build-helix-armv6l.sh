a4d70e2daefbbbb55939ec85c6a44e7bd6830847
Neil Pandya <neil@neilpandya.com>
2024-12-04 15:27:29 +0100

    Added environment variables to ensure static linking. Added verbose flag to help debug errors.

--- build-helix-armv6l.sh
+++ build-helix-armv6l.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-# Set CARGO_BUILD_JOBS environment variable to utilize 95% of available CPU cores for building 
-# Rust projects. Comment out or change to your discretion.
+# Set CARGO_BUILD_JOBS environment variable to utilize 95% of available CPU cores for building.
+# Comment out or change to your discretion.
 export CARGO_BUILD_JOBS=$(($(nproc) * 95 / 100))
 echo "CARGO_BUILD_JOBS set to $CARGO_BUILD_JOBS"
 
-# Verify and build for the required Rust target for ARMv6L (arm-unknown-linux-gnueabihf)
+# Set additional environment variables for static linking
+export RUSTFLAGS="-C target-feature=+crt-static"
+export OPENSSL_STATIC=1
+export OPENSSL_LIB_DIR=/usr/lib/arm-linux-gnueabi
+export OPENSSL_INCLUDE_DIR=/usr/include
+
+# Verify and build for the required Rust target
 rustup target add arm-unknown-linux-gnueabi && \
 echo "Installed Target(s):" && \
 rustup target list --installed && \
-cargo build \
+cargo build -vv \
     --target=arm-unknown-linux-gnueabi \
     --release
